6911986faff875e22df90ef2a76537b9ba1f1929
veronikachiara <156651567+veronikachiara@users.noreply.github.com>
2024-01-15 11:34:35 +0100

    Update RhymeWordExtractor.py

--- src/rhymewordextractor/RhymeWordExtractor.py
+++ src/rhymewordextractor/RhymeWordExtractor.py
@@ -13,9 +13,10 @@ class RhymeWordExtractor:
 
         returns:  -- (List of strings List of rhyme words extracted)'''
         # TBD
+
         rhymewordlist = []
         for verse in self.textlines:
             words = verse.split(' ')
             rhymeword = words[-1]
             rhymewordlist.append(rhymeword)
-        print(rhymewordlist)
+        return(rhymewordlist)
