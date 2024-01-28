9d9329acb8f48c026982ab27f9cbc450d7f85517
lisamg97 <lm.gebauer@gmx.de>
2024-01-28 02:01:56 +0100

    tei parser anfang

--- src/teiparser/TeiParser.py
+++ src/teiparser/TeiParser.py
@@ -1,4 +1,6 @@
-class TeiParser:
+from lxml import etree
+
+class TeiParser():
     '''TeiParser handles the reading of text from a TEI XML file'''
 
     def __init__(self, inputfile):
@@ -7,9 +9,29 @@ class TeiParser:
         inputfile:  -- (string) Path to TEI XML file to be read and parsed'''
         self.inputfile = inputfile
 
+       
+
     def parse(self):
         '''Parse textual content of internal TEI XML file
 
         returns:  -- (List of strings) All lines contained in TEI XML file (following P5 standard of TEI, usually identified by <l>-tags)'''
         # TBD
-        print("TeiParser hasn't been implemented yet.")
+        # Lisa
+
+        #load document
+        treeText = etree.parse(self.inputfile)
+
+        #namespace
+        ns = {'tei': 'http://www.tei-c.org/ns/1.0'}
+
+        #extract
+        lines = treeText.xpath('//tei:l', namespaces=ns)
+        result_lines = [line.text for line in lines if line.text is not None]
+
+        return result_lines
+
+
+
+
+
+    print("TeiParser is under construction. :)")
