1f37d0924be06719cb4ed412714d203c123b0ac6
Sheila Inc <sheeeshii.gaming@gmail.com>
2024-02-05 08:43:54 +0100

    Update txtparser.py
    Fertigstellung + Test

--- src/txtparser/TxtParser.py
+++ src/txtparser/TxtParser.py
@@ -3,14 +3,25 @@ class TxtParser:
 
     def __init__(self, inputfile):
         '''Create a new instance of TxtParser
-
         inputfile:  -- (string) Path to txt file to be read and parsed'''
         self.inputfile = inputfile
 
     def parse(self):
         '''Parse textual content of internal txt file
-
         returns:  -- (List of strings) All lines contained in txt file (single lines separated by linebreak in txt file)'''
 
-        # TBD Sheila
-        print("TxtParser hasn't been implemented yet.")
+        try:
+            with open(self.inputfile, 'r', encoding='utf-8') as file:
+                lines = file.readlines()
+                return lines
+        except FileNotFoundError:
+            print(f"File not found: {self.inputfile}")
+            return [] # leere Liste im Falle eines Fehlers
+
+# Test
+input_path = 'C:/Users/Sheilaa/Documents/GitHub/RimarioGen/src/txtparser/faust.txt'
+parser = TxtParser(input_path)
+parsed_lines = parser.parse()
+
+# Ausgabe
+print(parsed_lines)
