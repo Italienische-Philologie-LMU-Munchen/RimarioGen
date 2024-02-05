458e879bf6f372ef30605c1417d60860d51d3924
Sascha Resch <s.resch@42-projects.com>
2024-02-05 18:13:16 +0100

    Adjust TEI parser to handle subtags like <hi> for italics in verses

--- src/teiparser/TeiParser.py
+++ src/teiparser/TeiParser.py
@@ -1,5 +1,6 @@
 from lxml import etree
 
+
 class TeiParser():
     '''TeiParser handles the reading of text from a TEI XML file'''
 
@@ -9,28 +10,22 @@ class TeiParser():
         inputfile:  -- (string) Path to TEI XML file to be read and parsed'''
         self.inputfile = inputfile
 
-       
-
     def parse(self):
         '''Parse textual content of internal TEI XML file
 
         returns:  -- (List of strings) All lines contained in TEI XML file (following P5 standard of TEI, usually identified by <l>-tags)'''
-        
+
         # Lisa
 
-        #load document
+        # load document
         treeText = etree.parse(self.inputfile)
 
-        #namespace
+        # namespace
         ns = {'tei': 'http://www.tei-c.org/ns/1.0'}
 
-        #extract
+        # extract
         lines = treeText.xpath('//l', namespaces=ns)
-        result_lines = [line.text for line in lines if line.text is not None]
+        result_lines = [''.join(line.itertext())
+                        for line in lines if line.text is not None]
 
         return result_lines
-
-
-
-
-
