77fb386cd5a02bd314c5a56435fd003984280061
veronikachiara <156651567+veronikachiara@users.noreply.github.com>
2024-02-05 11:28:31 +0100

    Update RhymeWordExtractor.py

--- src/rhymewordextractor/RhymeWordExtractor.py
+++ src/rhymewordextractor/RhymeWordExtractor.py
@@ -19,7 +19,7 @@ class RhymeWordExtractor:
             words = verse.split(' ')
             rhymeword = words[-1]
             rhymeword = re.sub(r"^([^a-zA-Z])+", "", rhymeword)
-            rhymeword = re.sub(r"$([^a-zA-Z])+", "", rhymeword)
+            rhymeword = re.sub(r"([^a-zA-Z$])+", "", rhymeword)
             rhymewordlist.append(rhymeword)
         
         # re.sub(r"^\s*$", "", rhymewordlist)
