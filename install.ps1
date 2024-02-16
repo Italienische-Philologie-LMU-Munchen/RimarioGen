8d30cb855d22cc3394581edbfc81ec6a9f091697
Sascha Resch <s.resch@42-projects.com>
2024-02-16 12:27:45 +0100

    Added installation script for Windows

---
+++ install.ps1
@@ -0,0 +1,58 @@
+# Check for installed Python version
+$pythonVersion = python -V 2>&1 | %{$_.ToString().Trim()}
+if ($pythonVersion -match "Python (\d+\.\d+\.\d+)") {
+    $pythonVersion = $Matches[1]
+    Write-Host "Python version $pythonVersion is installed."
+} else {
+    Write-Host "Python is not installed. Please install Python and try again."
+    exit
+}
+
+# Install packages from requirements.txt using pip or pip3
+if ($pythonVersion -like "2.*") {
+    $pipCommand = "pip"
+} else {
+    $pipCommand = "pip3"
+}
+Write-Host "Installing packages from requirements.txt using $pipCommand..."
+$pipInstallCommand = "$pipCommand install -r requirements.txt"
+Invoke-Expression $pipInstallCommand
+
+# Copy files from specified directory to AppData folder
+$sourceDirectory = "$(Get-Location)\src"
+$destinationDirectory = "$env:APPDATA\RimarioGen"
+# Using https://devblogs.microsoft.com/powershell-community/determine-if-a-folder-exists/ 
+Write-Host "Check if there is a previous installation of Rimario Generator..."
+if (Test-Path -Path $destinationDirectory) 
+{
+    Write-Host "Emptying $destinationDirectory..."
+    # Using code from https://superuser.com/questions/1786887/how-to-delete-all-contents-of-a-folder-without-deleting-the-folder-itself
+    Get-ChildItem -Path $destinationDirectory | ForEach-Object -Process { 
+        If($_.attributes -eq "Directory"){
+            Remove-Item -Path $_.FullName -Recurse -Force;
+            }Else{
+            Remove-Item -Path $_.FullName -Force;};
+            };
+}
+else 
+{
+    Write-Host "Creating base directory $destinationDirectory..."
+    md -Path $destinationDirectory
+}
+Write-Host "Copying files from $sourceDirectory to $destinationDirectory..."
+Copy-Item -Path "$sourceDirectory\*" -Destination $destinationDirectory -Recurse
+
+# Create shortcut to start Rimario Generator on desktop
+# Using information from https://community.spiceworks.com/topic/2317514-powershell-how-can-i-change-the-icon-of-a-shortcut-to-a-custom-icon-file-i-have
+Write-Host "Creating a shortcut to Rimario Generator on Desktop..."
+$TargetFile = "$env:APPDATA\RimarioGen\rimarioGen.ps1"
+$desktopPath = [Environment]::GetFolderPath("Desktop")
+$shortcutFile = "$desktopPath\Rimario Generator.lnk"
+$WScriptShell = New-Object -ComObject WScript.Shell
+$shortcut = $WScriptShell.CreateShortcut($ShortcutFile)
+$shortcut.TargetPath = "powershell.exe"
+$shortcut.Arguments = "-NoExit -ExecutionPolicy Bypass -Command `"& '" + $TargetFile + "'`""
+$shortcut.IconLocation = "$env:APPDATA\RimarioGen\assets\clipboard-list-solid.ico"
+$shortcut.Save()
+
+Read-Host -Prompt "Press Enter to exit"
---
+++ src/rimarioGen.ps1
@@ -0,0 +1,47 @@
+# Check for installed Python version
+$pythonVersion = python -V 2>&1 | %{$_.ToString().Trim()}
+if ($pythonVersion -match "Python (\d+\.\d+\.\d+)") {
+    $pythonVersion = $Matches[1]
+    Write-Host "Python version $pythonVersion is installed."
+} else {
+    Write-Host "Python is not installed. Please install Python and try again."
+    exit
+}
+
+# Install packages from requirements.txt using pip or pip3
+if ($pythonVersion -like "2.*") {
+    $pythonCommand = "python.exe"
+} else {
+    $pythonCommand = "python.exe"
+}
+
+Write-Host "Starting Rimario generator..."
+
+$pythonRunCommand = "$env:APPDATA\RimarioGen\RimarioGen_Gui.py"
+Start-Process $pythonCommand "`"$pythonRunCommand`""
+
+Add-Type -AssemblyName UIAutomationClient
+
+do {
+    $MyProcessPy = Get-Process | Where-Object {$_.MainWindowTitle -like "*py.exe*"}
+    $MyProcessPython = Get-Process | Where-Object {$_.MainWindowTitle -like "*python.exe*"}
+} while ($MyProcessPy -eq $null -and $MyProcessPython -eq $null)
+$MyPowershellProcess = Get-Process -Id ([System.Diagnostics.Process]::GetCurrentProcess().Id)
+
+if (!($MyProcessPy -eq $null)) {
+    $MyProcess = $MyProcessPy
+}
+else {
+    $MyProcess = $MyProcessPython
+}
+
+$ae = [System.Windows.Automation.AutomationElement]::FromHandle($MyProcess.MainWindowHandle)
+$wp = $ae.GetCurrentPattern([System.Windows.Automation.WindowPatternIdentifiers]::Pattern)
+
+
+# Minimize python window so we only see GUI and no console window of python.exe.
+$IsMinimized = $wp.Current.WindowVisualState -eq 'Minimized'
+if (! $IsMinimized) { $wp.SetWindowVisualState('Minimized') } 
+
+# Close current window as we don't need powershell after startup anymore
+$MyPowershellProcess.Kill()
