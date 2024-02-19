c246c97c2221161794f67b0f71dcdb0117ef0506
Sascha Resch <s.resch@42-projects.com>
2024-02-19 12:06:28 +0100

    Update students involved

--- src/legalNoticeGui/LegalNoticeGui.py
+++ src/legalNoticeGui/LegalNoticeGui.py
@@ -46,7 +46,7 @@ Version: 0.1.0.0
 Copyright: ©2024, GNU GPLv3 License
 
 Insitute of Italian Philology, LMU Munich.
-Collaborators: Veronika Diem, Sheila Incarbone, Sascha Resch'''
+Collaborators: Veronika Diem, Sheila Incarbone, Daniel Rosenstein, Sascha Resch'''
         self.lbLegalText = Label(self, text=self.legalText, justify='left')
         self.lbLegalText.grid(row=1, column=0, padx=5, pady=5, sticky='NW')
 
